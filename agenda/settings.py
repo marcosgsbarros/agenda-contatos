a5c18bb186e5ccdf842537ab2502b734ae4aea86
marcosgsbarros <marcosgsbarros@gmail.com>
2023-04-22 23:15:02 -0300

    att

--- agenda/settings.py
+++ agenda/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'templates/static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates/static'
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
